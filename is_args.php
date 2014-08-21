ad17551b87ebc813c8613c004a8b0c9c08663377
ilkkak2 <ilkkakiviharju@ilkka-imac.elisa>
2014-08-21 11:04:33 +0300

    get script args
    Read args from GET, POST and command line

---
+++ is_args.php
@@ -0,0 +1,25 @@
+<?php
+//***********************************************
+//*
+//* Read all POST and GET variables
+//* and parse command line
+//*
+//***********************************************
+
+	date_default_timezone_set("Europe/Helsinki"); 
+
+	$prog_start_time = microtime_float();
+
+	if ($argv)
+		parse_str(implode('&', array_slice($argv, 1)), $_GET);
+
+	foreach ($_GET AS $key => $data)
+	{
+		$$key = $data;
+	}
+
+	foreach ($_POST AS $key => $data)
+	{
+		$$key = $data;
+	}
+?>
